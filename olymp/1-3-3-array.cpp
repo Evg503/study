485f9d128b0bbb438bdf6c9aff94bdf3ab424844
Evgeny Kochenyuk <evgeny.kochenyuk@gmail.com>
2023-06-18 22:36:00 +0200

    1.3.3 Array

---
+++ olymp/1-3-3-array.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	int m;
+	cin >> n >> m;
+	vector<int> v;
+    v.reserve(n);
+    for( int i = 0; i < n; ++i)
+    {
+        int a;
+        cin >>a;
+        v.push_back(a);
+    }
+	
+	for( int i = 0; i < m; ++i)
+	{
+		int l = -1;
+		int r = n;
+
+        int k;
+        cin >> k;
+
+		while (l+1<r)
+		{
+			int mid = (l+r)/2;
+
+			if (v[mid] < k)
+				l = mid;
+			else
+				r = mid;
+		}
+        if (r<n && v[r] == k)
+            cout << "YES\n";
+        else
+    		cout << "NO\n"; 
+	}
+}
